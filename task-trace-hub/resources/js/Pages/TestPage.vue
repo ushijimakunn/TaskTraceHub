e99f75928a8139ec2100fd19c2c891e836f9ad0b
ushijima eisaku <eisakuushijima@gmail.com>
2024-02-02 20:07:07 +0900

    end setup inertia vue3 laravel

---
+++ task-trace-hub/resources/js/Pages/TestPage.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+name: 'TestPage',
+}
+</script>
+
+<template>
+    <div>
+      <h1>Test Page</h1>
+      <p>This is a test page.</p>
+    </div>
+</template>
--- task-trace-hub/routes/web.php
+++ task-trace-hub/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('TestPage');
 });
